801bb49056433638ac032a079f4e5af3dec181e0
오건영 <118809296+o54711254@users.noreply.github.com>
2023-08-13 22:02:15 +0900

    [D2] Title: 달팽이 숫자, Time: 148 ms, Memory: 20,624 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -8,24 +8,26 @@ public class Solution {
 		int T = sc.nextInt();
 
 		for (int tc = 1; tc <= T; tc++) {
+
 			int N = sc.nextInt();
+
 			int[][] map = new int[N][N];
+
 			int r = 0;
 			int c = 0;
 
+			int dir = 0;
 			int[] dr = { 0, 1, 0, -1 };
 			int[] dc = { 1, 0, -1, 0 };
-			int dir = 0;
 
 			for (int i = 1; i <= N * N; i++) {
 				map[r][c] = i;
 
 				int nr = r + dr[dir];
 				int nc = c + dc[dir];
-
-				if (nr < N && nr >= 0 && nc < N && nc >= 0 && map[nr][nc] == 0) {
-					r = nr;
-					c = nc;
+				if (nc < N && nc >= 0 && nr < N && nr >= 0 && map[nr][nc] == 0) {
+					r = r + dr[dir];
+					c = c + dc[dir];
 				} else {
 					dir++;
 					dir = dir % 4;
@@ -33,8 +35,8 @@ public class Solution {
 					c = c + dc[dir];
 				}
 			}
-			System.out.println("#" + tc);
-			for(int i=0; i<map.length; i++) {
+			System.out.println("#"+tc);
+			for(int i =0; i<map.length; i++) {
 				for(int j =0; j<map.length; j++) {
 					System.out.print(map[i][j]+" ");
 				}
