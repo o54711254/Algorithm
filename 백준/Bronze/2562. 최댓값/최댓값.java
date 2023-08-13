b7560399a45c75df8fc4203ad830459c09c9df38
오건영 <118809296+o54711254@users.noreply.github.com>
2023-08-13 16:18:56 +0900

    [Bronze III] Title: 최댓값, Time: 108 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[9];
+		int max = 0;
+		for (int i = 0; i < 9; i++) {
+			arr[i] = sc.nextInt();
+			if(max<arr[i]) {
+				max = arr[i];
+			}
+		}
+		
+		for(int i =0; i<9; i++) {
+			if(max == arr[i]) {
+				System.out.println(max);
+				System.out.println(i+1);
+			}
+		}
+
+	}
+}
