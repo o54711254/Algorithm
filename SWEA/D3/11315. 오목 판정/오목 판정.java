f3964cf35cce30afcc39d143ba26d7cadef05c58
오건영 <118809296+o54711254@users.noreply.github.com>
2023-08-19 23:41:08 +0900

    [D3] Title: 오목 판정, Time: 155 ms, Memory: 26,204 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,88 +1,84 @@
+
 import java.util.Scanner;
- 
+
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
- 
-        int T = sc.nextInt(); // 테스트 케이스의 수
- 
-        for (int t = 1; t <= T; t++) {
-            int N = sc.nextInt(); // 판의 크기
- 
-            char[][] arr = new char[N][N];
-            for (int i = 0; i < N; i++) {
-                String s = sc.next();
-                arr[i] = s.toCharArray();
-            }
- 
-            int result = 0;
- 
-            // 수평 및 수직으로 패턴 확인
-            for (int i = 0; i < N; i++) {
-                int countHorizontal = 0;
-                int countVertical = 0;
- 
-                for (int j = 0; j < N; j++) {
-                    // 수평 확인
-                    if (arr[i][j] == 'o') {
-                        countHorizontal++;
-                        if (countHorizontal >= 5) {
-                            result++;
-                        }
-                    } else {
-                        countHorizontal = 0;
-                    }
- 
-                    // 수직 확인
-                    if (arr[j][i] == 'o') {
-                        countVertical++;
-                        if (countVertical >= 5) {
-                            result++;
-                        }
-                    } else {
-                        countVertical = 0;
-                    }
-                }
-            }
- 
-            // 대각선 확인
-            for (int i = 0; i <= N - 5; i++) {
-                for (int j = 0; j <= N - 5; j++) {
-                    int countDiagonalRight = 0;
-                    int countDiagonalLeft = 0;
- 
-                    // 대각선 오른쪽 아래 확인
-                    for (int k = 0; k < 5; k++) {
-                        if (arr[i + k][j + k] == 'o') {
-                            countDiagonalRight++;
-                            if (countDiagonalRight >= 5) {
-                                result++;
-                            }
-                        } else {
-                            countDiagonalRight = 0;
-                        }
-                    }
- 
-                    // 대각선 왼쪽 아래 확인
-                    for (int k = 0; k < 5; k++) {
-                        if (arr[i + k][j + 4 - k] == 'o') {
-                            countDiagonalLeft++;
-                            if (countDiagonalLeft >= 5) {
-                                result++;
-                            }
-                        } else {
-                            countDiagonalLeft = 0;
-                        }
-                    }
-                }
-            }
- 
-            // 결과 출력
-            if (result >= 1) {
-                System.out.println("#"+t+" "+"YES");
-            } else {
-                System.out.println("#"+t+" "+"NO");
-            }
-        }
-    }
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			int N = sc.nextInt();
+
+			char[][] arr = new char[N][N];
+
+			for (int i = 0; i < N; i++) {
+				String s = sc.next();
+				arr[i] = s.toCharArray();
+			}
+
+			int result = 0;
+
+			for (int i = 0; i < N; i++) {
+				int countHorizon = 0;
+				int countVertical = 0;
+
+				for (int j = 0; j < N; j++) {
+					if (arr[i][j] == 'o') {
+						countHorizon++;
+						if (countHorizon >= 5) {
+							result++;
+						}
+					} else {
+						countHorizon = 0;
+					}
+					if (arr[j][i] == 'o') {
+						countVertical++;
+						if (countVertical >= 5) {
+							result++;
+						}
+					} else {
+						countVertical = 0;
+					}
+				}
+			}
+
+			for (int i = 0; i <= N - 5; i++) {
+				for (int j = 0; j <= N - 5; j++) {
+
+					int DiagnolRight = 0;
+					int DiagnolLeft = 0;
+
+					for (int k = 0; k < 5; k++) {
+						if (arr[i + k][j + k] == 'o') {
+							DiagnolRight++;
+							if (DiagnolRight >= 5) {
+								result++;
+							}
+						} else {
+							DiagnolRight = 0;
+						}
+					}
+					for (int k = 0; k < 5; k++) {
+						if (arr[i + k][j + 4 - k] == 'o') {
+							DiagnolLeft++;
+							if (DiagnolLeft >= 5) {
+								result++;
+							}
+						} else {
+							DiagnolLeft = 0;
+						}
+					}
+				}
+			}
+			if (result >= 1) {
+				System.out.println("#" + tc + " " + "YES");
+			} else {
+				System.out.println("#" + tc + " " + "NO");
+			}
+
+		}
+	}
 }
