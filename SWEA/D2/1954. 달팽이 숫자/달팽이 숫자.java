eb16741a0c0c0dbfe69be631e1b7bd6ec2fed23e
오건영 <118809296+o54711254@users.noreply.github.com>
2023-08-19 19:55:50 +0900

    [D2] Title: 달팽이 숫자, Time: 148 ms, Memory: 20,624 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,6 +1,7 @@
+
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -8,24 +9,25 @@ public class Solution {
 		int T = sc.nextInt();
 
 		for (int tc = 1; tc <= T; tc++) {
-
 			int N = sc.nextInt();
 
-			int[][] map = new int[N][N];
+			int[][] arr = new int[N][N];
 
-			int r = 0;
-			int c = 0;
+			int r = 0, c = 0;
 
-			int dir = 0;
 			int[] dr = { 0, 1, 0, -1 };
 			int[] dc = { 1, 0, -1, 0 };
 
+			int dir = 0;
+
 			for (int i = 1; i <= N * N; i++) {
-				map[r][c] = i;
+
+				arr[r][c] = i;
 
 				int nr = r + dr[dir];
 				int nc = c + dc[dir];
-				if (nc < N && nc >= 0 && nr < N && nr >= 0 && map[nr][nc] == 0) {
+
+				if (nr < N && nr >= 0 && nc < N && nc >= 0 && arr[nr][nc] == 0) {
 					r = r + dr[dir];
 					c = c + dc[dir];
 				} else {
@@ -35,10 +37,10 @@ public class Solution {
 					c = c + dc[dir];
 				}
 			}
-			System.out.println("#"+tc);
-			for(int i =0; i<map.length; i++) {
-				for(int j =0; j<map.length; j++) {
-					System.out.print(map[i][j]+" ");
+			System.out.println("#" + tc);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(arr[i][j] + " ");
 				}
 				System.out.println();
 			}
