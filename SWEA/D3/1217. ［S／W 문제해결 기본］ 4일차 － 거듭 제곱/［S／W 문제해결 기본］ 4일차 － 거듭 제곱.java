a31e271a95e25706226653692066224b4177e66c
오건영 <118809296+o54711254@users.noreply.github.com>
2023-08-31 14:38:44 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 138 ms, Memory: 19,692 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,35 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
 		for (int tc = 1; tc <= 10; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-			int testC = sc.nextInt();
-			int N = sc.nextInt();
+			System.out.println("#" + tc+ " "+(int)Math.pow(N, M));
 
-			int M = sc.nextInt();
-
-			Stack<Integer> nums = new Stack<>();
-
-			for (int i = 0; i < M; i++) {
-				nums.push(N);
-			}
-
-			int[] arr = new int[nums.size()];
-			for (int i = 0; i < nums.size(); i++) {
-				arr[i] = nums.get(i);
-			}
-
-			int sum = 1;
-			for (int i = 0; i < arr.length; i++) {
-				sum *= arr[i];
-			}
-			System.out.println("#" + testC + " " + sum);
 		}
 	}
 }
