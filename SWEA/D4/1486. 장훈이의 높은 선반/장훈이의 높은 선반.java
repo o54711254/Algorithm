8e73947ce4d072082eb3255b688e21e5851942b1
오건영 <118809296+o54711254@users.noreply.github.com>
2023-09-04 15:47:49 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 229 ms, Memory: 23,364 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -4,40 +4,55 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int T, N, B, ans;
+	static int[] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
-
+		// 테스트케이스
+		T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 
 			st = new StringTokenizer(br.readLine());
+			// 점원의 수
+			N = Integer.parseInt(st.nextToken());
+			// 선반 높이
+			B = Integer.parseInt(st.nextToken());
 
-			int N = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
+			ans = 987654321;
 
-			int[] arr = new int[N];
-			int ans = 987654321;
-
+			// 키 배열에 값 넣기
+			arr = new int[N];
+			int rs = 0;
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
+				rs += arr[i];
 			}
 
-			for (int i = 0; i < (1 << N); i++) {
-				int sum = 0;
-				for (int j = 0; j < N; j++) {
-					if ((i & (1 << j)) != 0) {
-						sum += arr[j];
-					}
-				}
-				if (sum >= B && sum < ans) {
-					ans = sum;
-				}
-			}
-			System.out.println("#" + tc +" "+ (ans - B));
+			func(0, 0, rs);
 
+			System.out.println("#"+tc+" "+(ans-B));
 		}
+	}
+
+	static void func(int idx, int sum, int rsum) {
+		if (sum > ans) {
+			return;
+		}
+		if (sum + rsum < B) {
+			return;
+		}
+		if (idx == N) {
+			if (sum >= B) {
+				ans = Math.min(ans, sum);
+			}
+			return;
+		}
+
+		func(idx + 1, sum + arr[idx], rsum - arr[idx]);
+		func(idx + 1, sum, rsum - arr[idx]);
 	}
 }
