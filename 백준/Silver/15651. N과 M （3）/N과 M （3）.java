08f72a538b0b14444f349ec8eafc879a7a7efd0f
오건영 <118809296+o54711254@users.noreply.github.com>
2023-09-06 16:40:04 +0900

    [Silver III] Title: N과 M (3), Time: 592 ms, Memory: 312984 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] arr;
+	static int[] result;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+		result = new int[M];
+		sb = new StringBuilder();
+		permutation(0, M);
+		System.out.println(sb);
+	}
+
+	public static void permutation(int depth, int r) {
+		if (depth == r) {
+			for (int i = 0; i < r; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		int size = arr.length;
+		for (int i = 0; i < size; i++) {
+			result[depth] = arr[i];
+			permutation(depth + 1, r);
+		}
+	}
+}
