7c9b2fecc85c2397cf143f0fb27d530e8bcd0163
오건영 <118809296+o54711254@users.noreply.github.com>
2023-09-18 11:34:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 620 ms, Memory: 144784 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 	static boolean[] visit;
-	static int count;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,9 +17,8 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-
 		for (int i = 0; i < N + 1; i++) {
-			list.add(new ArrayList<>());
+			graph.add(new ArrayList<>());
 		}
 		visit = new boolean[N + 1];
 
@@ -27,25 +27,35 @@ public class Main {
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 
-			list.get(u).add(v);
-			list.get(v).add(u);
+			graph.get(u).add(v);
+			graph.get(v).add(u);
 		}
-		count = 0;
+		int count = 0;
 		for (int i = 1; i <= N; i++) {
 			if(!visit[i]) {
-				dfs(i);
+				bfs(i);
 				count++;
 			}
+
 		}
 		System.out.println(count);
+
 	}
 
-	static void dfs(int vertex) {
+	static void bfs(int vertex) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(vertex);
 		visit[vertex] = true;
-		for (int i = 0; i < list.get(vertex).size(); i++) {
-			int newVertex = list.get(vertex).get(i);
-			if (!visit[newVertex]) {
-				dfs(newVertex);
+
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			for (int i = 0; i < graph.get(now).size(); i++) {
+				int next = graph.get(now).get(i);
+				if (!visit[next]) {
+					queue.add(next);
+					visit[next] = true;
+				}
+
 			}
 		}
 	}
