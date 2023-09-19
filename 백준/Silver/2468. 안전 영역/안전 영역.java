2a938bd6ce3e4d7a934c361e2bc088597a915f46
오건영 <118809296+o54711254@users.noreply.github.com>
2023-09-19 12:51:20 +0900

    [Silver I] Title: 안전 영역, Time: 264 ms, Memory: 53564 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,78 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] map;
+	static boolean[][] visit;
+	static int N;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+
+		map = new int[N][N];
+
+		int max = 0;
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (max <= map[i][j]) {
+					max = map[i][j];
+				}
+			}
+		}
+
+		int[] arr = new int[max+1];
+
+		for (int k = 0; k <= max; k++) {
+			visit = new boolean[N][N];
+			int count = 0;
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] - k >= 0 && !visit[i][j]) {
+						bfs(new Point(i, j), k);
+						count++;
+					}
+				}
+			}
+			arr[k] = count;
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-1]);
+
+	}
+
+	public static void bfs(Point p, int k) {
+		Queue<Point> queue = new LinkedList<>();
+		queue.add(p);
+		visit[p.x][p.y] = true;
+
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int nr = now.x + dr[i];
+				int nc = now.y + dc[i];
+				if (nr < 0 || nc < 0 || nr >= N || nc >= N || map[nr][nc] - k < 0 || visit[nr][nc]) {
+					continue;
+				}
+				queue.add(new Point(nr, nc));
+				visit[nr][nc] = true;
+			}
+
+		}
+
+	}
+}
