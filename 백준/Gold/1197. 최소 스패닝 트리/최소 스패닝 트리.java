34f2045c49627b631135e17ccbc9da4379cc7b3c
오건영 <118809296+o54711254@users.noreply.github.com>
2023-09-24 20:20:34 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 684 ms, Memory: 55744 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Node {
+	int v;
+	int w;
+
+	public Node(int v, int w) {
+		super();
+		this.v = v;
+		this.w = w;
+	}
+}
+
+public class Main {
+	static ArrayList<ArrayList<Node>> graph;
+	static boolean[] visit;
+	static int sum;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		graph = new ArrayList<ArrayList<Node>>();
+		visit = new boolean[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			graph.add(new ArrayList<Node>());
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+
+			graph.get(A).add(new Node(B, C));
+			graph.get(B).add(new Node(A, C));
+		}
+		sum = 0;
+		prim(new Node(1, 0));
+		System.out.println(sum);
+	}
+
+	public static void prim(Node start) {
+		PriorityQueue<Node> queue = new PriorityQueue<Node>(new Comparator<Node>() {
+
+			@Override
+			public int compare(Node o1, Node o2) {
+				// TODO Auto-generated method stub
+				return o1.w - o2.w;
+			}
+		});
+		queue.add(start);
+
+		while (!queue.isEmpty()) {
+			Node now = queue.poll();
+			if (!visit[now.v]) {
+				sum += now.w;
+				visit[now.v] = true;
+				for (int i = 0; i < graph.get(now.v).size(); i++) {
+					Node next = graph.get(now.v).get(i);
+					queue.add(next);
+				}
+			}
+		}
+	}
+}
