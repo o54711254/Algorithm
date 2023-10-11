b00c5b0c8fa12a6f09b1635139ffac82deb5433c
오건영 <118809296+o54711254@users.noreply.github.com>
2023-10-11 21:08:39 +0900

    [Gold IV] Title: 감시, Time: 248 ms, Memory: 92004 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -5,134 +5,132 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class CCTV {
-    int x;
-    int y;
-    int type;
+	int x;
+	int y;
+	int type;
 
-    public CCTV(int x, int y, int type) {
-        super();
-        this.x = x;
-        this.y = y;
-        this.type = type;
-    }
+	public CCTV(int x, int y, int type) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
 }
 
 public class Main {
-    static int[] dr = { 0, 1, 0, -1 };
-    static int[] dc = { 1, 0, -1, 0 };
-    static int[][] map;
-    static int N, M;
-    static int min = Integer.MAX_VALUE;
-    static ArrayList<CCTV> cctvList = new ArrayList<CCTV>();
+	static int N, M;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, -1, 0, 1 };
+	static int[][] map;
+	static int result = Integer.MAX_VALUE;
+	static ArrayList<CCTV> cctvList = new ArrayList<CCTV>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] > 0 && map[i][j] != 6) {
-                    cctvList.add(new CCTV(i, j, map[i][j]));
-                }
-            }
-        }
+		map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] > 0 && map[i][j] != 6) {
+					cctvList.add(new CCTV(i, j, map[i][j]));
+				}
+			}
+		}
+		dfs(0);
+		System.out.println(result);
+	}
 
-        dfs(0);
-        System.out.println(min);
-    }
+	static void dfs(int depth) {
+		if (depth == cctvList.size()) {
+			result = Math.min(result, check());
+			return;
+		}
+		int[][] copy = new int[N][M];
+		CCTV c = cctvList.get(depth);
+		switch (c.type) {
+		case 1:
+			for (int dir = 0; dir < 4; dir++) {
+				copy(copy, map);
+				watch(c.x, c.y, dir);
+				dfs(depth + 1);
+				copy(map, copy);
+			}
+			break;
+		case 2:
+			for (int dir = 0; dir < 2; dir++) {
+				copy(copy, map);
+				watch(c.x, c.y, dir);
+				watch(c.x, c.y, (dir + 2) % 4);
+				dfs(depth + 1);
+				copy(map, copy);
+			}
+			break;
+		case 3:
+			for (int dir = 0; dir < 4; dir++) {
+				copy(copy, map);
+				watch(c.x, c.y, dir);
+				watch(c.x, c.y, (dir + 1) % 4);
+				dfs(depth + 1);
+				copy(map, copy);
+			}
+			break;
+		case 4:
+			for (int dir = 0; dir < 4; dir++) {
+				copy(copy, map);
+				watch(c.x, c.y, dir);
+				watch(c.x, c.y, (dir + 1) % 4);
+				watch(c.x, c.y, (dir + 2) % 4);
+				dfs(depth + 1);
+				copy(map, copy);
+			}
+			break;
+		case 5:
+			for (int dir = 0; dir < 4; dir++) {
+				copy(copy, map);
+				watch(c.x, c.y, dir);
+			}
+			dfs(depth + 1);
+			copy(map, copy);
+			break;
 
-    public static void dfs(int depth) {
-        if (depth == cctvList.size()) {
-            min = Math.min(min, check());
-            return;
-        }
+		}
+	}
 
-        CCTV c = cctvList.get(depth);
-        int copy[][] = new int[N][M];
+	static void watch(int x, int y, int dir) {
+		while (true) {
+			x += dr[dir];
+			y += dc[dir];
+			if (x < 0 || y < 0 || x >= N || y >= M || map[x][y] == 6) {
+				break;
+			}
+			if (map[x][y] == 0) {
+				map[x][y] = -1;
+			}
+		}
+	}
 
-        switch (c.type) {
-            case 1:
-                for (int dir = 0; dir < 4; dir++) {
-                    copyMap(copy, map);
-                    watch(c.x, c.y, dir);
-                    dfs(depth + 1);
-                    copyMap(map, copy);
-                }
-                break;
-            case 2:
-                for (int dir = 0; dir < 2; dir++) {
-                    copyMap(copy, map);
-                    watch(c.x, c.y, dir);
-                    watch(c.x, c.y, dir + 2);
-                    dfs(depth + 1);
-                    copyMap(map, copy);
-                }
-                break;
-            case 3:
-                for (int dir = 0; dir < 4; dir++) {
-                    copyMap(copy, map);
-                    watch(c.x, c.y, dir);
-                    watch(c.x, c.y, (dir + 1) % 4);
-                    dfs(depth + 1);
-                    copyMap(map, copy);
-                }
-                break;
-            case 4:
-                for (int dir = 0; dir < 4; dir++) {
-                    copyMap(copy, map);
-                    watch(c.x, c.y, dir);
-                    watch(c.x, c.y, (dir + 1) % 4);
-                    watch(c.x, c.y, (dir + 2) % 4);
-                    dfs(depth + 1);
-                    copyMap(map, copy);
-                }
-                break;
-            case 5:
-                for (int dir = 0; dir < 4; dir++) {
-                    copyMap(copy, map);
-                    watch(c.x, c.y, dir);
-                }
-                dfs(depth + 1);
-                copyMap(map, copy);
-                break;
-        }
-    }
+	static int check() {
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 0) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
 
-    static void watch(int x, int y, int dir) {
-        while (true) {
-            x += dr[dir];
-            y += dc[dir];
-            if (x < 0 || y < 0 || x >= N || y >= M || map[x][y] == 6) {
-                break;
-            }
-            if (map[x][y] == 0) {
-                map[x][y] = -1;
-            }
-        }
-    }
-
-    static int check() {
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) {
-                    sum++;
-                }
-            }
-        }
-        return sum;
-    }
-
-    static void copyMap(int[][] dest, int[][] src) {
-        for (int i = 0; i < N; i++) {
-            dest[i] = src[i].clone();
-        }
-    }
+	static void copy(int[][] copy, int[][] map) {
+		for (int i = 0; i < N; i++) {
+			copy[i] = map[i].clone();
+		}
+	}
 }
