56bbaaf331956f6bb0d3b83dd394e42c306757a4
오건영 <118809296+o54711254@users.noreply.github.com>
2023-10-11 21:49:30 +0900

    [Gold III] Title: 사다리 조작, Time: 288 ms, Memory: 18908 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -1,89 +1,85 @@
 import java.awt.Point;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int M, K, N;
-	static int[][] map;
-	static boolean[][] ladder;
-	static int[] dr = { 1, 0, 0 };
-	static int[] dc = { 0, 1, -1 };
-	static int answer = 4;
+	private static int M, K, N, ans;
+	private static int[][] map;
+	private static boolean finish = false;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 
 		map = new int[N + 1][M + 1];
-		ladder = new boolean[N + 1][M + 1];
 
+		int x, y;
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			ladder[a][b] = true;
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			map[x][y] = 1;
+			map[x][y + 1] = 2;
 		}
 
-		dfs(1, 0);
-		 System.out.println((answer != 4) ? answer : -1);
+		for (int i = 0; i <= 3; i++) {
+			ans = i;
+			dfs(1, 0);
+			if (finish) {
+				break;
+			}
+		}
 
+		System.out.println((finish) ? ans : -1);
 	}
 
 	private static void dfs(int x, int count) {
-		if (answer <= count)
+		if (finish) {
 			return;
-		else {
+		}
+		if (ans == count) {
 			if (check()) {
-				answer = count;
-				return;
+				finish = true;
 			}
+			return;
 		}
 
 		for (int i = x; i < N + 1; i++) {
 			for (int j = 1; j < M; j++) {
-				if (!ladder[i][j]) {
-					ladder[i][j] = true;
+				if (map[i][j] == 0 && map[i][j + 1] == 0) {
+					map[i][j] = 1;
+					map[i][j + 1] = 2;
 					dfs(i, count + 1);
-					ladder[i][j] = false;
+					map[i][j] = map[i][j + 1] = 0;
 				}
 			}
 		}
 	}
 
 	static boolean check() {
-		int cnt = 0;
-
 		for (int i = 1; i <= M; i++) {
-			if (i == bfs(new Point(1, i))) {
-				cnt++;
+			if (i != move(new Point(1, i))) {
+				return false;
 			}
 		}
-
-		if (M == cnt) {
-			return true;
-		} else {
-			return false;
-		}
-
+		return true;
 	}
 
-	static int bfs(Point p) {
+	static int move(Point p) {
 		while (p.x <= N) {
-			if (ladder[p.x][p.y]) {
+			if (map[p.x][p.y] == 1) {
 				p.y++;
-			} else if (p.y > 1 && ladder[p.x][p.y - 1]) {
+			} else if (map[p.x][p.y] == 2) {
 				p.y--;
 			}
 			p.x++;
 		}
 		return p.y;
+
 	}
 }
