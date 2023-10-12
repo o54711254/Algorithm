9ca0a981ea9231ce5bf53cc84dbfbce2142b85aa
오건영 <118809296+o54711254@users.noreply.github.com>
2023-10-12 10:05:50 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,85 +1,76 @@
 import java.awt.Point;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
- 
- 
+
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static ArrayList<Point> person;
-    static ArrayList<Point> chicken;
-    static int ans;
-    static boolean[] open;
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
- 
-        map = new int[N][N];
-        person = new ArrayList<>();
-        chicken = new ArrayList<>();
- 
-        // 미리 집과 치킨집에 해당하는 좌표를 ArrayList에 넣어 둠.
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
- 
-                if (map[i][j] == 1) {
-                    person.add(new Point(i, j));
-                } else if (map[i][j] == 2) {
-                    chicken.add(new Point(i, j));
-                }
-            }
-        }
- 
-        ans = Integer.MAX_VALUE;
-        open = new boolean[chicken.size()];
- 
-        DFS(0, 0);
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
- 
-    public static void DFS(int start, int cnt) {
-        if (cnt == M) {
-            int res = 0;
- 
-            for (int i = 0; i < person.size(); i++) {
-                int temp = Integer.MAX_VALUE;
- 
-                // 어떤 집과 치킨집 중 open한 치킨집의 모든 거리를 비교한다.
-                // 그 중, 최소 거리를 구한다.
-                for (int j = 0; j < chicken.size(); j++) {
-                    if (open[j]) {
-                        int distance = Math.abs(person.get(i).x - chicken.get(j).x)
-                                + Math.abs(person.get(i).y - chicken.get(j).y);
- 
-                        temp = Math.min(temp, distance);
-                    }
-                }
-                res += temp;
-            }
-            ans = Math.min(ans, res);
-            return;
-        }
- 
-        // 백트래킹
-        for (int i = start; i < chicken.size(); i++) {
-            open[i] = true;
-            DFS(i + 1, cnt + 1);
-            open[i] = false;
-        }
-    }
- 
+	static int N, M;
+	static int[][] map;
+	static ArrayList<Point> house;
+	static ArrayList<Point> chicken;
+	static int ans;
+	static boolean[] check;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new int[N][N];
+		house = new ArrayList<>();
+		chicken = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+
+				if (map[i][j] == 1) {
+					house.add(new Point(i, j));
+				} else if (map[i][j] == 2) {
+					chicken.add(new Point(i, j));
+				}
+			}
+		}
+
+		ans = Integer.MAX_VALUE;
+		check = new boolean[chicken.size()];
+		
+		dfs(0, 0);
+		System.out.println(ans);
+
+	}
+
+	static void dfs(int start, int depth) {
+		if (depth == M) {
+			int result = 0;
+
+			for (int i = 0; i < house.size(); i++) {
+				int temp = Integer.MAX_VALUE;
+
+				for (int j = 0; j < chicken.size(); j++) {
+					if (check[j]) {
+						int distance = Math.abs(house.get(i).x - chicken.get(j).x)
+								+ Math.abs(house.get(i).y - chicken.get(j).y);
+						temp = Math.min(temp, distance);
+					}
+				}
+				result += temp;
+			}
+			ans = Math.min(ans, result);
+			return;
+		}
+
+		for (int i = start; i < chicken.size(); i++) {
+			check[i] = true;
+			dfs(i + 1, depth + 1);
+			check[i] = false;
+
+		}
+	}
 }
