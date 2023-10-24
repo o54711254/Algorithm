70c70ab7350374be519965b78d03b4c64b6cb867
오건영 <118809296+o54711254@users.noreply.github.com>
2023-10-24 10:09:26 +0900

    [Gold IV] Title: 타임머신, Time: 240 ms, Memory: 16792 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,111 +1,97 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-class Edge{
-	int s,e,cost;
-	public Edge(int s, int e, int cost) {
-		this.s = s;
-		this.e = e;
+class Edge {
+	int start;
+	int end;
+	int cost;
+
+	public Edge(int start, int end, int cost) {
+		super();
+		this.start = start;
+		this.end = end;
 		this.cost = cost;
 	}
+
+	@Override
+	public String toString() {
+		return "Edge [start=" + start + ", end=" + end + ", cost=" + cost + "]";
+	}
+
 }
 
 public class Main {
-	// 변수
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N,M;
+	static int N, M;
+	static int INF = 200000000;
 	static long[] dist;
-	static ArrayList<Edge> graph;
-	static int INF = 1234567890;
-	
-	// main
+	static ArrayList<Edge> edges;
+
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
 		st = new StringTokenizer(br.readLine());
-		N = input(st); // 도시 수 (정점)
-		M = input(st); // 버스 수 (간선)
-		
-		// 거리 배열 초기화
-		dist = new long[N+1];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		edges = new ArrayList<>();
+		dist = new long[N + 1];
 		Arrays.fill(dist, INF);
 		dist[1] = 0;
-		
-		// 간선 정보 입력
-		graph = new ArrayList<>();
-		for(int i=0; i<M; i++) {
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int A = input(st); // 시작
-			int B = input(st); // 도착
-			int C = input(st); // 비용
-			
-			graph.add(new Edge(A,B,C));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			edges.add(new Edge(a, b, c));
 		}
-		
-		if(!bellmanFord()) {
+
+		if (!bellmanFord()) {
 			System.out.println(-1);
+		} else {
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 2; i <= N; i++) {
+				if (dist[i] == INF) {
+
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(dist[i]).append("\n");
+				}
+			}
+			System.out.println(sb);
 		}
-		else {
-			print();
-		}
-		
+
 	}
-	
-	// 벨만 포드 알고리즘
+
 	static boolean bellmanFord() {
-		// 정점 개수만큼 반복
-		for(int i=0; i<N-1; i++) {
-			// 간선 개수만큼 반복
-			for(int k=0; k<M; k++) {
-				Edge edge = graph.get(k); // 현재 간선
-				
-				if(dist[edge.s]!=INF) {
-					if(dist[edge.e] > dist[edge.s] + edge.cost) {
-						dist[edge.e] = dist[edge.s] + edge.cost;
+		// 정점의 개수만큼
+		for (int i = 0; i < N; i++) {
+			// 간선의 개수만큼
+			for (int j = 0; j < M; j++) {
+				Edge edge = edges.get(j);
+
+				if (dist[edge.start] != INF) {
+					if (dist[edge.end] > dist[edge.start] + edge.cost) {
+						dist[edge.end] = dist[edge.start] + edge.cost;
 					}
 				}
 			}
 		}
-		
+
 		// 음수 가중치 확인
-		for(int i=0; i<M; i++) {
-			Edge edge = graph.get(i);
-			
-			if(dist[edge.s]!=INF) {
-				if(dist[edge.e] > dist[edge.s] + edge.cost) {
+		// 값이 바뀌면 false
+		for (int i = 0; i < M; i++) {
+			Edge edge = edges.get(i);
+
+			if (dist[edge.start] != INF) {
+				if (dist[edge.end] > dist[edge.start] + edge.cost) {
 					return false;
 				}
 			}
 		}
 		return true;
-		
-	}
-	
-	static void print() {
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=2; i<=N; i++) {
-			if(dist[i]==INF) {
-				sb.append(-1).append("\n");
-			}
-			else {
-				sb.append(dist[i]).append("\n");
-			}
-		}
-		
-		System.out.println(sb);
-		
-	}
-	
-	// ---------------
-	static int input() throws IOException {
-		return Integer.parseInt(br.readLine());
-	}
-	
-	static int input(StringTokenizer st) {
-		return Integer.parseInt(st.nextToken());
 	}
 }
