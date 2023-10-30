87cc757a0a281d682b267b9831c6386b0cabcddc
오건영 <118809296+o54711254@users.noreply.github.com>
2023-10-30 16:04:59 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 1876 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,84 +2,87 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node {
-	int x;
-	int y;
-	int w;
-
-	public Node(int x, int y, int w) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.w = w;
-	}
-}
-
 public class Main {
-	static int[][] map;
-	static int[][] dist;
-	static boolean[][] visit;
+	static int N, cnt;
+	static int[][] map, weights;
+	static boolean[][] visited;
 	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static final int INF = 200000000;
-	static int N;
+	static int[] dc = { 0, 0, 1, -1 };
+
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int tc = 0;
+
+		cnt = 0;
 		while (true) {
-			tc++;
-			N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			N = Integer.parseInt(st.nextToken());
+
 			if (N == 0) {
 				break;
 			}
+
 			map = new int[N][N];
-			dist = new int[N][N];
-			visit = new boolean[N][N];
+			weights = new int[N][N];
+			visited = new boolean[N][N];
+
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(weights[i], Integer.MAX_VALUE);
+			} // 최댓값으로 채워주기
+
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					dist[i][j] = INF;
+				}
+			} // 입력 끝
+
+			// 시작점은 가중치 넣어주기
+			weights[0][0] = map[0][0];
+
+			for (int t = 0; t < N * N; t++) {
+				// 배열 돌면서 방문하지 않은 곳 중에서 가중치가 가장 작은 칸을 찾아
+				int min = Integer.MAX_VALUE;
+				Point find = null; // 가장 작은 칸의 행,열 좌표
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						if (!visited[i][j] && weights[i][j] < min) {
+							min = weights[i][j];
+							find = new Point(i, j);
+						}
+					}
+				}
+
+				visited[find.x][find.y] = true; // 방문표시
+				for (int d = 0; d < 4; d++) {
+					int nextR = find.x + dr[d];
+					int nextC = find.y + dc[d];
+
+					if (nextR >= 0 && nextR < N && nextC >= 0 && nextC < N && !visited[nextR][nextC]) {
+						if (weights[find.x][find.y] + map[nextR][nextC] < weights[nextR][nextC]) {
+							weights[nextR][nextC] = weights[find.x][find.y] + map[nextR][nextC];
+						}
+					}
 				}
 			}
-			bfs(new Node(0, 0, 0));
 
-			System.out.println("Problem " + tc + ": " + dist[N - 1][N - 1]);
+			// 출력
+			cnt++;
+			System.out.println("Problem " + cnt + ": " + weights[N - 1][N - 1]);
+
 		}
 
-	}
+	}// main
 
-	public static void bfs(Node start) {
-		PriorityQueue<Node> queue = new PriorityQueue<Node>(new Comparator<Node>() {
-
-			@Override
-			public int compare(Node o1, Node o2) {
-				return o1.w - o2.w;
-			}
-		});
-		queue.add(start);
-		dist[start.x][start.y] = map[start.x][start.y];
-
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-			visit[now.x][now.y] = true;
-			for (int i = 0; i < 4; i++) {
-				int nr = now.x + dr[i];
-				int nc = now.y + dc[i];
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N || visit[nr][nc]) {
-					continue;
-				}
-				if (dist[nr][nc] > dist[now.x][now.y] + map[nr][nc]) {
-					dist[nr][nc] = dist[now.x][now.y] + map[nr][nc];
-					queue.add(new Node(nr, nc, dist[nr][nc]));
-				}
-			}
-		}
-	}
 }
