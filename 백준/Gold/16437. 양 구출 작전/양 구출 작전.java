bff7f8a22b70b4355b0ece70d97ef0933c75570f
오건영 <118809296+o54711254@users.noreply.github.com>
2023-11-03 10:32:26 +0900

    [Gold III] Title: 양 구출 작전, Time: 1116 ms, Memory: 100760 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -2,71 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class island {
-	char animal;
-	int number;
-
-	public island(char animal, int number) {
-		super();
-		this.animal = animal;
-		this.number = number;
-
-	}
-
-}
-
 public class Main {
-	static ArrayList<ArrayList<Integer>> graph;
-	static island[] islands;
 	static int N;
+	static ArrayList<ArrayList<Integer>> list;
+	static long[] nums;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(br.readLine());
-		islands = new island[N + 1];
+		N = Integer.parseInt(st.nextToken());
 
-		graph = new ArrayList<>();
+		nums = new long[N + 1]; // 동물의 수를 저장할 배열
+		list = new ArrayList<>(); // 간선을 저장할 리스트
 		for (int i = 0; i < N + 1; i++) {
-			graph.add(new ArrayList<>());
+			list.add(new ArrayList<>());
 		}
-		islands[1] = new island('S', 0);
 
 		for (int i = 2; i < N + 1; i++) {
 			st = new StringTokenizer(br.readLine());
-			char ani = st.nextToken().charAt(0);
-			int num = Integer.parseInt(st.nextToken());
-			int con = Integer.parseInt(st.nextToken());
+			String ani = st.nextToken();
+			int mari = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
 
-			graph.get(con).add(i);
-			islands[i] = new island(ani, num);
+			list.get(n).add(i); // n번 노드에 노드 i번 넣기, 단방향으로 넣어줘도 충분
+
+			if (ani.equals("W"))
+				mari *= -1; // 늑대이면 마릿수를 음수로 넣기
+			nums[i] = mari; // nums 배열에 동물의 수 저장
+
 		}
 
-		System.out.println(dfs(1));
+		dfs(1, -1);
+		System.out.println(nums[1]);
 
-	}
+	}// main
 
-	static long dfs(int index) {
-
-		long result = 0; // 살아남은 양의 수
-
-		for (int i = 0; i < graph.get(index).size(); i++) {
-			result += dfs(graph.get(index).get(i));
+	static void dfs(int bottom, int up) {
+		// 먼저 노드 끝까지 들어가
+		for (int x : list.get(bottom)) {
+			// bottom 이였던 애가 이제는 up이 됨
+			dfs(x, bottom);
 		}
 
-		if (islands[index].animal == 'S') {
-			result += islands[index].number;
-		} else {
-			result -= islands[index].number;
+		if (up != -1) {
+			if (nums[bottom] > 0) {
+				nums[up] += nums[bottom];
+			}
 		}
 
-		if (result < 0) {
-			result = 0;
-		}
-		return result;
+	}// dfs
 
-	}
 }
