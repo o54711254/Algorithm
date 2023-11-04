5bf61b1c74e3416918216d387907e210a0a4850d
오건영 <118809296+o54711254@users.noreply.github.com>
2023-11-04 19:46:42 +0900

    [Gold I] Title: 구간 합 구하기, Time: 528 ms, Memory: 116744 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, K;
+	static long[] arr;
+	static long[] tree;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		M += K;
+
+		arr = new long[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Long.parseLong(br.readLine());
+		}
+
+		int h = (int) Math.ceil(Math.log(N) / Math.log(2));
+		int size = (int) Math.pow(2, h + 1);
+
+		tree = new long[size];
+		init(1, 0, N - 1);
+
+		StringBuilder sb = new StringBuilder();
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int what = Integer.parseInt(st.nextToken());
+			if (what == 1) {
+				int index = Integer.parseInt(st.nextToken());
+				long val = Long.parseLong(st.nextToken());
+				update(1, 0, N - 1, index - 1, val);
+
+			} else {
+				int left = Integer.parseInt(st.nextToken());
+				int right = Integer.parseInt(st.nextToken());
+				sb.append(query(1, 0, N - 1, left - 1, right - 1)).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+	static void init(int node, int start, int end) {
+		if (start == end) {
+			tree[node] = arr[start];
+		} else {
+			init(node * 2, start, (start + end) / 2);
+			init(node * 2 + 1, (start + end) / 2 + 1, end);
+			tree[node] = tree[node * 2] + tree[node * 2 + 1];
+		}
+	}
+
+	static long query(int node, int start, int end, int left, int right) {
+		if (left > end || right < start) {
+			return 0;
+		}
+		if (left <= start && end <= right) {
+			return tree[node];
+		}
+		long lsum = query(node * 2, start, (start + end) / 2, left, right);
+		long rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+		return lsum + rsum;
+	}
+
+	static void update(int node, int start, int end, int index, long val) {
+		if (index < start || index > end) {
+			return;
+		}
+		if (start == end) {
+			arr[index] = val;
+			tree[node] = val;
+			return;
+		}
+		update(node * 2, start, (start + end) / 2, index, val);
+		update(node * 2 + 1, (start + end) / 2 + 1, end, index, val);
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+}
