7c64b8f128427bdc339afa8ccb06123a366d5f59
오건영 <118809296+o54711254@users.noreply.github.com>
2023-11-29 20:40:35 +0900

    [Gold V] Title: 적록색약, Time: 96 ms, Memory: 12788 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,4 +1,4 @@
-import java.awt.Point;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,95 +7,79 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static char[][] map;
-	static char[][] map2;
-	static boolean[][] visit;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static int normal;
-	static int handi;
+    static int N, cnt, cntRG;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static char[][] map;
+    static boolean[][] visit;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		map = new char[N][N];
-		map2 = new char[N][N];
-		visit = new boolean[N][N];
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        visit = new boolean[N][N];
 
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < N; j++) {
-				map[i][j] = str.charAt(j);
-				map2[i][j] = str.charAt(j);
-				if (map2[i][j] == 'G') {
-					map2[i][j] = 'R';
-				}
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            map[i] = str.toCharArray();
+        }
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j]) {
+                    bfs(new Point(i, j));
+                    cnt++;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 'G') {
+                    map[i][j] = 'R';
+                }
+            }
+        }
+        visit = new boolean[N][N];
 
-		normal = 0;
-		handi = 0;
-		// 노말
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j] == 'R' && !visit[i][j]) {
-					bfs('R', new Point(i, j), map);
-					normal++;
-				}
-				if (map[i][j] == 'G' && !visit[i][j]) {
-					bfs('G', new Point(i, j), map);
-					normal++;
-				}
-				if (map[i][j] == 'B' && !visit[i][j]) {
-					bfs('B', new Point(i, j), map);
-					normal++;
-				}
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j]) {
+                    bfs(new Point(i, j));
+                    cntRG++;
+                }
+            }
+        }
 
-		visit = new boolean[N][N];
+        System.out.println(cnt + " " + cntRG);
+    }
 
-		// 장애인
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map2[i][j] == 'R' && !visit[i][j]) {
+    static void bfs(Point p) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(p);
+        visit[p.x][p.y] = true;
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nr = now.x + dr[i];
+                int nc = now.y + dc[i];
+                if (nr < 0 || nc < 0 || nr >= N || nc >= N || visit[nr][nc] || map[nr][nc] != map[p.x][p.y]) {
+                    continue;
+                }
+                queue.add(new Point(nr, nc));
+                visit[nr][nc] = true;
+            }
+        }
+    }
 
-					bfs('R', new Point(i, j), map2);
-					handi++;
-				}
-
-				if (map2[i][j] == 'B' && !visit[i][j]) {
-					bfs('B', new Point(i, j), map2);
-					handi++;
-
-				}
-			}
-		}
-
-		System.out.println(normal + " " + handi);
-
-	}
-
-	public static void bfs(char c, Point p, char[][] a) {
-		Queue<Point> queue = new LinkedList<Point>();
-		queue.add(p);
-		visit[p.x][p.y] = true;
-		int N = map.length;
-
-		while (!queue.isEmpty()) {
-			Point now = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int nr = now.x + dr[i];
-				int nc = now.y + dc[i];
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N || a[nr][nc] != c || visit[nr][nc]) {
-					continue;
-				}
-				queue.add(new Point(nr, nc));
-				visit[nr][nc] = true;
-			}
-		}
-	}
+    static void print() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
