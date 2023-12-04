bc8302b8c5bb28a0adb725fe192414511a24c8d0
오건영 <118809296+o54711254@users.noreply.github.com>
2023-12-04 23:08:25 +0900

    [Gold IV] Title: 감시, Time: 204 ms, Memory: 26788 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -38,6 +38,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                // 만약 0이 아니고 6(벽)이 아니면 cctv 이므로 넣어줌
                 if (map[i][j] != 0 && map[i][j] != 6) {
                     cctvList.add(new CCTV(i, j, map[i][j]));
                 }
@@ -51,20 +52,23 @@ public class Main {
     }
 
     static void dfs(int depth) {
+        // cctv의 갯수
         int size = cctvList.size();
 
         if (depth == size) {
+            // check 메서드로 경우의 수 별 사각지대의 갯수를 구하고 최솟값을 구함
             result = Math.min(result, check());
             return;
         }
 
+        // 복사용 copy배열
         int[][] copy = new int[N][M];
 
-
         CCTV now = cctvList.get(depth);
-
+        // cctv의 타입에 따라
         switch (now.type) {
             case 1:
+                // dir은 방향, 1번 카메라는 4개의 경우의 수
                 for (int dir = 0; dir < 4; dir++) {
                     copy(map, copy);
                     move(now, dir);
@@ -73,7 +77,7 @@ public class Main {
                 }
                 break;
             case 2:
-                for (int dir = 0; dir < 4; dir++) {
+                for (int dir = 0; dir < 2; dir++) {
                     copy(map, copy);
                     move(now, dir);
                     move(now, (dir + 2) % 4);
