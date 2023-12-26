63ac7d979827880fad1cf7c6606ca54510ae12eb
오건영 <118809296+o54711254@users.noreply.github.com>
2023-12-26 18:46:48 +0900

    [Gold IV] Title: 네트워크 연결, Time: 492 ms, Memory: 50460 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -2,77 +2,76 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node {
-	int v;
-	int w;
+// 가중치가 작은 순서부터 정렬
+class Node implements Comparable<Node> {
+    int v;
+    int w;
 
-	public Node(int v, int w) {
-		super();
-		this.v = v;
-		this.w = w;
-	}
+    public Node(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.w - o.w;
+    }
 }
 
-public class Main {
-	static ArrayList<ArrayList<Node>> graph;
-	static boolean[] visit;
-	static int sum;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+class Main {
+    static int sum;
+    static ArrayList<ArrayList<Node>> graph;
+    static boolean[] visit;
 
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		graph = new ArrayList<ArrayList<Node>>();
-		visit = new boolean[N + 1];
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N + 1; i++) {
-			graph.add(new ArrayList<Node>());
-		}
+        graph = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+        visit = new boolean[N + 1];
 
-			graph.get(a).add(new Node(b, c));
-			graph.get(b).add(new Node(a, c));
-		}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-		sum = 0;
+            graph.get(a).add(new Node(b, c));
+            graph.get(b).add(new Node(a, c));
+        }
 
-		prim(new Node(1, 0));
-		System.out.println(sum);
-	}
+        prim(new Node(1,0));
+        System.out.println(sum);
 
-	public static void prim(Node start) {
-		PriorityQueue<Node> queue = new PriorityQueue<Node>(new Comparator<Node>() {
+    }
 
-			@Override
-			public int compare(Node o1, Node o2) {
-				// TODO Auto-generated method stub
-				return o1.w - o2.w;
-			}
-		});
-		queue.add(start);
-
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-			if (!visit[now.v]) {
-				visit[now.v] = true;
-				sum += now.w;
-				for (int i = 0; i < graph.get(now.v).size(); i++) {
-					Node next = graph.get(now.v).get(i);
-					queue.add(next);
-				}
-			}
-		}
-	}
+    static void prim(Node start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(start);
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            if (visit[now.v]) {
+                continue;
+            }
+            visit[now.v] = true;
+            sum += now.w;
+            for (int i = 0; i < graph.get(now.v).size(); i++) {
+                Node next = graph.get(now.v).get(i);
+                if(!visit[next.v]){
+                    pq.add(next);
+                }
+            }
+        }
+    }
 }
