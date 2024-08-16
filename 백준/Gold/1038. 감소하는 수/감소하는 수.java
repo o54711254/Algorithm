0ca83044ea430d4f250b28c7c91d5b1d4b3a0bee
오건영 <o54711254@gmail.com>
2024-08-16 22:34:08 +0900

    [Gold V] Title: 감소하는 수, Time: 68 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,40 +1,44 @@
 import java.io.*;
 import java.util.*;
+import java.awt.*;
 
 class Main {
-    static int N;
-    static ArrayList<Long> pointList;
+	static int N;
+	static int result;
+	static ArrayList<Long> pointList;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        pointList = new ArrayList<>();
+		N = Integer.parseInt(br.readLine());
+		pointList = new ArrayList<>();
 
-        if (N < 10) {
-            System.out.println(N);
-            return;
-        } else if (N >= 1023) {  // 감소하는 수는 최대 1022개
-            System.out.println(-1);
-            return;
-        }
+		if (N < 10) {
+			System.out.println(N);
+			return;
+		} else if (N >= 1023) {
+			System.out.println(-1);
+			return;
+		}
 
-        for (int i = 0; i < 10; i++) {
-            search(1, i);
-        }
+		for (int i = 0; i < 10; i++) {
+			search(1, i);
+		}
 
-        Collections.sort(pointList);
+		Collections.sort(pointList);
 
-        System.out.println(pointList.get(N));
-    }
+		System.out.println(pointList.get(N));
 
-    static void search(int idx, long number) {
-        if (idx > 10) {
-            return;
-        }
-        pointList.add(number);
-        for (int i = 0; i < number % 10; i++) {
-            search(idx + 1, number * 10 + i);
-        }
-    }
+	}
+
+	static void search(int idx, long number) {
+		if (idx > 10) {
+			return;
+		}
+		pointList.add(number);
+		for (int i = 0; i < number % 10; i++) {
+			search(idx + 1, number * 10 + i);
+		}
+	}
 }
