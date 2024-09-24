c82d7e38ab3c964f5a6abdf2b4b71389350bff4c
오건영 <o54711254@gmail.com>
2024-09-24 19:51:43 +0900

    [Gold IV] Title: 미로만들기, Time: 72 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -2,73 +2,59 @@ import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] map;
-	static int[][] dist;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static int N;
-	static boolean flag;
-	static int ans;
-	static final int INF = 200000000;
+    static int map[][];
+    static int dr[] = { 0, 1, 0, -1 };
+    static int dc[] = { 1, 0, -1, 0 };
+    static int dist[][];
+    static int N;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        dist = new int[N][N];
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j) - '0';
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        bfs(new Point(0, 0));
 
-		map = new int[N][N];
-		dist = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < N; j++) {
-				map[i][j] = str.charAt(j) - '0';
-				dist[i][j] = INF;
-			}
-		}
-		bfs(new Point(0, 0));
-		System.out.println(dist[N - 1][N - 1]);
-	}
+        System.out.println(dist[N - 1][N - 1]);
+    }
 
-	static void bfs(Point p) {
-		Queue<Point> queue = new LinkedList<Point>();
-		queue.add(p);
-		dist[p.x][p.y] = 0;
+    public static void bfs(Point p) {
+        Deque<Point> deque = new LinkedList<>();
+        deque.addFirst(p);
+        dist[p.x][p.y] = 0;
 
-		while (!queue.isEmpty()) {
-			Point now = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int nr = now.x + dr[i];
-				int nc = now.y + dc[i];
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-					continue;
-				}
-				if (dist[nr][nc] > dist[now.x][now.y]) {
-					if (map[nr][nc] == 1) {
-						dist[nr][nc] = dist[now.x][now.y];
-					} else {
-						dist[nr][nc] = dist[now.x][now.y] + 1;
-					}
-					queue.add(new Point(nr, nc));
-				}
-
-			}
-		}
-
-	}
-
-	static void print() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print(dist[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+        while (!deque.isEmpty()) {
+            Point now = deque.pollFirst();
+            for (int i = 0; i < 4; i++) {
+                int nr = now.x + dr[i];
+                int nc = now.y + dc[i];
+                if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
+                    continue;
+                }
+                int cost = (map[nr][nc] == 1) ? 0 : 1;
+                if (dist[nr][nc] > dist[now.x][now.y] + cost) {
+                    dist[nr][nc] = dist[now.x][now.y] + cost;
+                    if (cost == 0) {
+                        deque.addFirst(new Point(nr, nc));
+                    } else {
+                        deque.addLast(new Point(nr, nc));
+                    }
+                }
+            }
+        }
+    }
 }
