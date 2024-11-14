a327a83a51ffbfcb6aadefd2be014fa2375a1f8e
오건영 <o54711254@gmail.com>
2024-11-14 17:01:26 +0900

    [Gold V] Title: 노드사이의 거리, Time: 212 ms, Memory: 37428 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+class Node {
+	int num;
+	int distance;
+
+	public Node(int num, int distance) {
+		this.num = num;
+		this.distance = distance;
+	}
+}
+
+class Root {
+	int start;
+	int end;
+
+	public Root(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+}
+
+class Main {
+	static int N;
+	static int M;
+
+	static ArrayList<ArrayList<Node>> list = new ArrayList<ArrayList<Node>>();
+	static ArrayList<Root> rootList = new ArrayList<Root>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N + 1; i++) {
+			list.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int distance = Integer.parseInt(st.nextToken());
+
+			list.get(start).add(new Node(end, distance));
+			list.get(end).add(new Node(start, distance));
+		}
+
+		sb = new StringBuilder();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(findDistance(start, end));
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
+	static int findDistance(int start, int end) {
+		int result = 0;
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(start);
+
+		int[] arr = new int[N + 1];
+		fillArrMaxValue(arr);
+
+		arr[start] = 0;
+
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			for (int i = 0; i < list.get(now).size(); i++) {
+				Node next = list.get(now).get(i);
+				if (arr[next.num] > arr[now] + next.distance) {
+					arr[next.num] = arr[now] + next.distance;
+					if (next.num == end) {
+						return arr[next.num];
+					} else {
+						queue.add(next.num);
+					}
+				}
+			}
+		}
+		result = arr[end];
+
+		return result;
+	}
+
+	static void fillArrMaxValue(int[] arr) {
+		Arrays.fill(arr, Integer.MAX_VALUE);
+	}
+}
