3f7ef361f0d001e502ede487e3eebdeb326892bd
오건영 <o54711254@gmail.com>
2024-12-16 15:33:53 +0900

    [Gold V] Title: A와 B, Time: 64 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,22 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-/*
- * B
- * BA
- * ABB
- * ABBA
- * <==>
- * 1. 문자열 뒤에 A를 뺀다
- * 2. 문자열 뒤의 B를 빼고 뒤집는다.
- * LinkedList로 deque 구현 
- * 뒤집는건 boolean flag를 통해 구현  
- * 
- * 1. ABBA 
- * 2. ABB (removeLast)
- * 3. AB(= BA) (removeLast, false) 
- * 4. */
-
 class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -37,16 +21,17 @@ class Main {
 		boolean flag = true;
 
 		while (!deque.isEmpty() && deque.size() != start.length()) {
-            if (flag) {
-                if (deque.removeLast() == 'B') {
-                    flag = false; // 뒤집기 상태로 전환
-                }
-            } else {
-                if (deque.removeFirst() == 'B') {
-                    flag = true; // 정방향 상태로 전환
-                }
-            }
-        }
+
+			if (flag) {
+				if (deque.removeLast() == 'B') {
+					flag = !flag;
+				}
+			} else {
+				if (deque.removeFirst() == 'B') {
+					flag = !flag;
+				}
+			}
+		}
 
 		StringBuilder sb = new StringBuilder();
 		while (!deque.isEmpty()) {
@@ -57,10 +42,6 @@ class Main {
 			}
 		}
 
-		if (sb.toString().equals(start)) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
+		System.out.println(sb.toString().equals(start) ? 1 : 0);
 	}
 }
