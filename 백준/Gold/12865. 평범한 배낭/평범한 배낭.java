cc98a11ca4ec3a9b65cda821c86bb77ef2c749fd
오건영 <o54711254@gmail.com>
2025-02-21 19:58:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 12228 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,42 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Object {
+	int weight;
+	int value;
+
+	public Object(int weight, int value) {
+		this.weight = weight;
+		this.value = value;
+	}
+}
+
+class Main {
+	static int N, K;
+	static int[] dp;
+	static List<Object> list = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-		int[] w = new int[N + 1];
-		int[] v = new int[N + 1];
-		int[][] dp = new int[N + 1][K + 1];
+		dp = new int[K + 1];
 
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
+			int weight = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
 
-			w[i] = Integer.parseInt(st.nextToken());
-			v[i] = Integer.parseInt(st.nextToken());
+			list.add(new Object(weight, value));
 		}
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= K; j++) {
-				
-				if (w[i] > j) {
-					dp[i][j] = dp[i-1][j];
-					
-				} else {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-				}
+		knapsack();
+		System.out.println(dp[K]);
+
+	}
+
+	static void knapsack() {
+		for (Object object : list) {
+			int weight = object.weight;
+			int value = object.value;
+
+			for (int i = K; i - weight >= 0; i--) {
+				dp[i] = Math.max(dp[i], dp[i - weight] + value);
 			}
 		}
-		System.out.println(dp[N][K]);
 	}
 }
