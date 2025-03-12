2e56edbdf07ad126e314d91b5fad1ed465211df0
오건영 <o54711254@gmail.com>
2025-03-12 21:34:55 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = my_string.substring(0, n);
+        return answer;
+    }
+}
