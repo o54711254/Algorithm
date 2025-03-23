403e00a5c5a6365cebac48fd8c4a7d0df06fcc22
오건영 <o54711254@gmail.com>
2025-03-23 19:21:18 +0900

    [level 0] Title: 0 떼기, Time: 0.12 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        while(n_str.startsWith("0")){
+            n_str = n_str.replaceFirst("0", "");
+        }
+        return n_str;
+    }
+}
