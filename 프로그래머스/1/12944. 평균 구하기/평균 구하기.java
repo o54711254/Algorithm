ea9e982a36ef17b92c7bc971790407d1880340b0
오건영 <o54711254@gmail.com>
2025-03-29 17:25:47 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int num : arr){
+            answer += num;
+        }
+        
+        answer = answer/arr.length;
+        return answer;
+    }
+}
