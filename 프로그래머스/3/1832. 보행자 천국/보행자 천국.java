e82e96d4f405f33414baa6fb2467ca62dc2e7218
오건영 <o54711254@gmail.com>
2025-03-30 17:34:34 +0900

    [level 3] Title: 보행자 천국, Time: 152.58 ms, Memory: 98.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    int MOD = 20170805;
+    public int solution(int m, int n, int[][] cityMap) {
+        int[][][] dp = new int[m+1][n+1][2];
+        dp[0][0][0] = 1;
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(cityMap[i][j] == 0){
+                    dp[i+1][j][0] = (dp[i][j][0] + dp[i][j][1])%MOD;
+                    dp[i][j+1][1] = (dp[i][j][0] + dp[i][j][1])%MOD;
+                }else if(cityMap[i][j] == 2){
+                    dp[i+1][j][0] = dp[i][j][0];
+                    dp[i][j+1][1] = dp[i][j][1];
+                }
+            }
+        }
+        
+        int answer = (dp[m-1][n-1][0]+dp[m-1][n-1][1])%MOD;
+        return answer;
+    }
+}
